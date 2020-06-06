420931dd0243e1984bc7ec90cd9b39e4571f8f4f
Matt Chapman <matt.c@clubware.co.nz>
2020-06-07 00:11:23 +1200

    Add Spotify widget

---
+++ spotify.jsx
@@ -0,0 +1,24 @@
+import { baseStyles } from "./lib/styles";
+
+// Pinched command from https://github.com/ThomasBrace/Ubersicht-Spotify-widget/blob/master/SpotifyCurrentTrack.coffee
+const command = `
+IFS='|' read -r theArtist theName <<<"$(osascript <<<'tell application "Spotify"
+        set theTrack to current track
+        set theArtist to artist of theTrack
+        set theName to name of theTrack
+        return theArtist & "|" & theName
+    end tell')"
+echo "$theArtist - $theName"`;
+
+const refreshFrequency = 5000;
+
+const style = {
+  marginTop: "3px",
+  left: "10px",
+};
+
+const render = ({ output }) => (
+  <div style={{ ...baseStyles, ...style }}>♫ {output}</div>
+);
+
+export { command, refreshFrequency, render };
