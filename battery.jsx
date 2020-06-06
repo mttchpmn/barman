aba6b66d2a9ef783e89f3154d554e4a33d12c784
Matt Chapman <matt.c@clubware.co.nz>
2020-06-06 23:49:05 +1200

    Update batt widget

---
+++ battery.jsx
@@ -0,0 +1,43 @@
+import { baseStyles } from "./lib/styles";
+import { colors } from "./lib/theme";
+
+const command = "./barman/lib/battery.sh";
+
+const style = {
+  marginTop: "3px",
+  right: "250px",
+};
+
+const render = ({ output }) => {
+  let [percentage, remaining, charging] = output.split(" ");
+  percentage = parseInt(percentage, 10);
+  charging = charging === "true";
+  return (
+    <div style={{ ...baseStyles, ...style }}>
+      {charging ? (
+        <span
+          style={{
+            color: colors.cyan,
+          }}
+        >
+          {percentage}% | charging...
+        </span>
+      ) : (
+        <span
+          style={{
+            color:
+              percentage > 30
+                ? colors.green
+                : percentage > 15
+                ? colors.orange
+                : colors.red,
+          }}
+        >
+          {percentage}% | {remaining} left
+        </span>
+      )}
+    </div>
+  );
+};
+
+export { command, render };
