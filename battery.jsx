43b121b28d0425f3f4a55954ab106bcc718454bc
Matt Chapman <matt.c@clubware.co.nz>
2020-06-07 13:23:03 +1200

    Update battery styling

--- battery.jsx
+++ battery.jsx
@@ -2,42 +2,73 @@ import { baseStyles } from "./lib/styles";
 import { colors } from "./lib/theme";
 
 const command = "./barman/lib/battery.sh";
-
 const refreshFrequency = 1000;
+
+const containerStyle = {
+  backgroundColor: colors.currentLine,
+  borderBottomLeftRadius: "3px",
+  borderBottomRightRadius: "3px",
+  height: "20px",
+  right: "200px",
+  width: "70px",
+};
 
 const style = {
   marginTop: "3px",
-  right: "250px",
+  marginLeft: "10px",
 };
 
 const render = ({ output }) => {
   let [percentage, charging, remaining] = output.trim().split(" ");
   percentage = parseInt(percentage, 10);
   charging = charging === "true";
+
+  const Charging = () => (
+    <span
+      style={{
+        color: colors.cyan,
+      }}
+    >
+      <i class="fas fa-plug"></i> {percentage}%
+    </span>
+  );
+
+  const getBatteryIconClass = (charge) => {
+    if (charge === 100) {
+      return "fas fa-battery-full";
+    } else if (charge > 75) {
+      return "fas fa-battery-three-quarters";
+    } else if (charge > 50) {
+      return "fas fa-battery-half";
+    } else if (charge > 25) {
+      return "fas fa-battery-quarter";
+    } else {
+      return "fas fa-battery-empty";
+    }
+  };
+
+  const Battery = () => (
+    <span
+      style={{
+        color:
+          percentage > 30
+            ? colors.green
+            : percentage > 15
+            ? colors.orange
+            : colors.red,
+      }}
+    >
+      <i
+        style={{ transform: "rotate(270deg)" }}
+        class={getBatteryIconClass(percentage)}
+      ></i>{" "}
+      {percentage}%{/* {percentage}% | {remaining || "??"} left */}
+    </span>
+  );
+
   return (
-    <div style={{ ...baseStyles, ...style }}>
-      {charging ? (
-        <span
-          style={{
-            color: colors.cyan,
-          }}
-        >
-          {percentage}% | charging...
-        </span>
-      ) : (
-        <span
-          style={{
-            color:
-              percentage > 30
-                ? colors.green
-                : percentage > 15
-                ? colors.orange
-                : colors.red,
-          }}
-        >
-          {percentage}% | {remaining || "??"} left
-        </span>
-      )}
+    <div style={{ ...baseStyles, ...containerStyle }}>
+      <div style={{ ...style }}>{charging ? <Charging /> : <Battery />}</div>
     </div>
   );
 };
