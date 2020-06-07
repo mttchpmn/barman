69e046926b2847be8958cd46b311091baf6cff9a
Matt Chapman <matt.c@clubware.co.nz>
2020-06-07 13:24:00 +1200

    Update batt script

--- lib/battery.sh
+++ lib/battery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # BATTERY
-BATT_PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -c -2)
+BATT_PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -c -3)
 BATT_REMAINING=$(pmset -g batt | grep -Eo "\d+:\d+")
 BATT_CHARGING="false"
 
